e1eb781aa802048b499f09d45ce42ad29da8680d
Dhun Chhabra <133389465+Dhun04@users.noreply.github.com>
2025-02-20 19:35:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'; // Import Tailwind CSS styles
-import LoginPage from './pages/LoginPage';
-import TaskFormPage from './pages/TaskFormPage';
-import HomePage from './pages/HomePage';
-
-const MainApp = () => { // Renamed the local App component to MainApp
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/add-task" element={<TaskFormPage />} />
-      </Routes>
-    </Router>
-  );
-};
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainApp /> {/* Use the renamed MainApp component */}
+    <App />
   </React.StrictMode>
 );
